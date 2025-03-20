852815aa5a21bb2a4cd8d1aab2572ab75c1db390
Bamidele Ogunseye <bambamogunseye@gmail.comgit config --user.name bamidele>
2025-03-20 13:31:05 -0500

    cors fix

--- ServerSide/index.js
+++ ServerSide/index.js
@@ -21,7 +21,7 @@ app.use(express.json({limit: '50mb'}));
 app.use (express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin:"https://whatsthetea.netlify.app"||"http://localhost:5173",
    credentials: true,
 }))
 
