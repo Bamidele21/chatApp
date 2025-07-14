6142fce80b307e2cff901e51ba900c0d1ab17ea5
Bamidele Ogunseye <bambamogunseye@gmail.comgit config --user.name bamidele>
2025-07-14 12:11:34 -0500

    changes

--- ClientSide/src/components/sidebar.jsx
+++ ClientSide/src/components/sidebar.jsx
@@ -23,11 +23,11 @@ function Sidebar () {
                 key={user._id}
                 onClick={()=>{setSelectedUsers(user)}}>
                     <img className={onlineUsers.includes(user._id)?"profile-img online":"profile-img"}
-                    src={user.profilePic || "/Profile_avatar_placeholder_large.png"} height="100" width= "100"
+                        src={user.profilePic || "/Profile_avatar_placeholder_large.png"} height="100" width= "100"
                     alt={user.name}/>
                     <div className="user-info">
-                    <p>{user.fullName}</p>
-                    {onlineUsers.includes(user._id)? 
+                        <p>{user.fullName}</p>
+                        {onlineUsers.includes(user._id)? 
                         <span style={{color: "green"}}>online!</span>:"offline"
                     }
                     </div>
